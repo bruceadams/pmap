6c121d15021d5d6cea91bcc5f7fdfeafbb91d2c7
Bruce Adams <adams@vivisimo.com>
2010-10-29 13:51:42 -0400

    Implement limited thread pool size. Support Ruby 1.8.x.

--- lib/pmap.rb
+++ lib/pmap.rb
@@ -3,21 +3,33 @@
 # "Pmap" into "Enumerable". I haven't figured out how to do it.
 # So, I directly reopen "Enumerable" and add "p" methods...
 
+require 'thread' unless defined?(Mutex)
+
 module Enumerable
-  # FIXME: Add support for a limited thread pool size.
-  def pmap(thread_count=nil, &b)
+  def pmap(tcount=nil, &b)
+    # This seems overly fussy... (code smell)
     in_array = self.to_a        # I'm not sure how expensive this is...
-    out_array = Array.new(in_array.size)
-    threads = (0...in_array.size).map {|j|
-      Thread.new(j) {|x| out_array[x] = yield(in_array[x])}
+    my_size = in_array.size
+    out_array = Array.new(my_size)
+    semaphore = Mutex.new
+    index = -1                  # Our use of index is protected by semaphore
+    threads = (0...(tcount || my_size)).map {
+      Thread.new {
+        i = nil
+        while (semaphore.synchronize {i = (index += 1)}; i < my_size)
+          out_array[i] = yield(in_array[i])
+        end
+      }
     }
     threads.each {|t| t.join}
     out_array
   end
 
-  # This is doing some extra work: building a return array that we throw away.
-  def peach(thread_count=nil, &b)
-    pmap(&b)
+  # This is doing some extra work: building a return array that is
+  # thrown away.  How can I share the core code of "pmap" here,
+  # omitting the output array creation?
+  def peach(tcount=nil, &b)
+    pmap(tcount, &b)
     self
   end
 end
