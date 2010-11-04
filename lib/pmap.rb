90686c71b07b72238c9fe72962ace416b1954930
Bruce Adams <bruce.adams@acm.org>
2010-11-04 18:58:26 -0400

    Add limited size thread pool

--- lib/pmap.rb
+++ lib/pmap.rb
@@ -5,18 +5,23 @@
 
 require 'thread' unless defined?(Mutex)
 
+$pmap_default_thread_count ||= 64
+
 module Enumerable
-  def pmap(tcount=nil, &b)
+  def pmap(thread_count=nil, &proc)
+    raise ArgumentError, "thread_count must be at least one." unless
+      thread_count.nil? or (thread_count.respond_to?(:>=) and thread_count >= 1)
     # This seems overly fussy... (code smell)
     in_array = self.to_a        # I'm not sure how expensive this is...
-    my_size = in_array.size
-    out_array = Array.new(my_size)
+    size = in_array.size
+    thread_count = [thread_count||$pmap_default_thread_count, size].min
+    out_array = Array.new(size)
     semaphore = Mutex.new
     index = -1                  # Our use of index is protected by semaphore
-    threads = (0...(tcount || my_size)).map {
+    threads = (0...thread_count).map {
       Thread.new {
         i = nil
-        while (semaphore.synchronize {i = (index += 1)}; i < my_size)
+        while (semaphore.synchronize {i = (index += 1)}; i < size)
           out_array[i] = yield(in_array[i])
         end
       }
@@ -26,10 +31,10 @@ module Enumerable
   end
 
   # This is doing some extra work: building a return array that is
-  # thrown away.  How can I share the core code of "pmap" here,
-  # omitting the output array creation?
-  def peach(tcount=nil, &b)
-    pmap(tcount, &b)
+  # thrown away.  How can I share the core code of "pmap" here and
+  # omit the output array creation?
+  def peach(thread_count=nil, &proc)
+    pmap(thread_count, &proc)
     self
   end
 end
