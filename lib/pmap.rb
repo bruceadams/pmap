82c2812101cf881cd2fa1af95968da199f62d419
David Biehl <dbiehl@ncmedical.com>
2015-11-11 10:37:11 -0800

    Shows the method documentation on rubydoc.info

--- lib/pmap.rb
+++ lib/pmap.rb
@@ -5,16 +5,74 @@ require "pmap/thread_pool"
 $pmap_default_thread_count ||= 64
 
 module PMap
+  # @!method pmap
+  #
+  #   Parallel #map for any Enumerable.
+  #
+  #   When a block is given, each item is yielded to the block in a
+  #   separate thread. When no block is given, an Enumerable is
+  #   returned.
+  #
+  #   @see http://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-map
+  #
+  #   @param thread_count [Integer] maximum number of threads to create
+  #     (optional)
+  #
+  #   @return [Array] of mapped objects
+  #
+
+  # @!method peach
+  #
+  #   Parallel #each for any Enumerable.
+  #
+  #   When a block is given, each item is yielded to the block in a
+  #   separate thread. When no block is given, an Enumerable is
+  #   returned.
+  #
+  #   @see http://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-each
+  #
+  #   @param thead_count [Integer] maximum number of threads to create
+  #     (optional)
+  #
+  #   @return [void]
+  #
+
+  # @!method peach_with_index
+  #
+  #   Parallel #each_with_index for any Enumerable.
+  #
+  #   When a block is given, each item is yielded to the block in a
+  #   separate thread. When no block is given, an Enumerable is
+  #   returned.
+  #
+  #   @see http://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-each_with_index
+  #
+  #   @param thread_count [Integer] maximum number of threads to create
+  #     (optional)
+  #
+  #   @return [void]
+  #
+
+  # @!method flat_pmap
+  #
+  #   Parallel #flat_map for any Enumerable
+  #
+  #   When a block is given, each item is yielded to the block in a
+  #   separate thread. When no block is given, an Enumerable is
+  #   returned.
+  #
+  #   @see http://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-flat_map
+  #
+  #   @param thread_count [Integer] maximum number of threads to create
+  #     (optional)
+  #
+  #   @return [Array] of mapped objects
+  #
+
   def self.included(base)
     base.class_eval do
 
-      # Public: Parallel "map" for any Enumerable.
-      #
-      # When a block is given, each item is yielded to the block in a separate
-      # thread. When no block is given, an Enumerable is returned.
-      #
-      # thread_count - maximum number of threads to create (optional)
-      #
+      # @see PMap#pmap
       def pmap(thread_count=nil, &proc)
         return self unless proc
 
@@ -27,13 +85,7 @@ module PMap
         end
       end
 
-      # Public: Parallel "each" for any Enumerable.
-      #
-      # When a block is given, each item is yielded to the block in a separate
-      # thread. When no block is given, an Enumerable is returned.
-      #
-      # thread_count - maximum number of threads to create (optional)
-      #
+      # @see PMap#peach
       def peach(thread_count=nil, &proc)
         if proc
           peach_with_index(thread_count) do |item, index|
@@ -43,13 +95,7 @@ module PMap
         self
       end
 
-      # Public: Parallel "each_with_index" for any Enumerable
-      #
-      # When a block is given, each item is yielded to the block in a separate
-      # thread. When no block is given, an Enumerable is returned.
-      #
-      # thread_count - maximum number of threads to create (optional)
-      #
+      # @see PMap#peach_with_index
       def peach_with_index(thread_count=nil, &proc)
         return each_with_index unless proc
 
@@ -63,16 +109,10 @@ module PMap
         self
       end
 
-      # Public: Parallel "flat_map" for any Enumerable
-      #
-      # When a block is given, each item is yielded to the block in a separate
-      # thread. When no block is given, an Enumerable is returned.
-      #
-      # thread_count - maximum number of threads to create (optional)
-      #
+      # @see PMap#flat_pmap
       def flat_pmap(thread_count=nil, &proc)
         return self unless proc
-        
+
         pmap(thread_count, &proc).flatten(1)
       end
     end
